ca0a75b6e60ba52f61b8b19f1256f7d2f63854e5
Max-Grabov <maxpgrabov@gmail.com>
2024-10-23 11:05:13 -0400

    Updated my java version to utilize BigInteger

--- Cryptography/HW5/primrootcheck.java
+++ Cryptography/HW5/primrootcheck.java
@@ -1,17 +1,19 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Scanner;
 class PrimRootCheck {
     public static void main(String[] args) {
         //Variable setup and setup code for algorithms
         int n;
-        long p, g;
+        BigInteger p = new BigInteger("0");
+        BigInteger g = new BigInteger("0");
         Scanner s = new Scanner(System.in);
 
         n = s.nextInt();
 
         for(int i = 0; i < n; i++){
-            p = s.nextLong();
-            g = s.nextLong();
+            p = s.nextBigInteger();
+            g = s.nextBigInteger();
 
             if(isPrimitiveRoot(p, g) == true){
                 System.out.println(1);
@@ -24,26 +26,34 @@ class PrimRootCheck {
         s.close();
     }
 
-    public static boolean isPrimitiveRoot(long p, long g){
+    public static boolean isPrimitiveRoot(BigInteger p, BigInteger g){
         //First we need to determine the divisors of p
-        ArrayList<Long> factors = new ArrayList<>();
+        ArrayList<BigInteger> factors = new ArrayList<>();
 
+        BigInteger check = p.subtract(BigInteger.ONE);
         //In my proof for Number 7, I showed that we only need to look at factors that are prime numbers
         //This is because that is where a non-primitive roots' cycles will first appear.
         //However, for the sake of this problem, I will check all factors since i am kinda lazy
-        for(int i = 2; i * i < p - 1; i++){
-            if((p - 1) % i == 0){
-                //Add both the number and the value that multiplies said number to p - 1
-                factors.add(Long.valueOf(i));
-                factors.add(Long.valueOf(p / i));
+        BigInteger i = BigInteger.TWO;
+        while(true){
+            if(i.multiply(i).compareTo(check) == 1){
+                break;
             }
+
+            if(check.mod(i).equals(BigInteger.ZERO)){
+                //Add both the number and the value that multiplies said number to p - 1
+                factors.add(i);
+                factors.add(check.divide(i));
+            }
+
+            i = i.add(BigInteger.ONE);
         }
 
         long numFactors = factors.size();
 
         //If we have a remainder of 1, there is a cycle therefore g is not a primitive root of p
-        for(int i = 0; i < numFactors; i++){
-            if(modExp(g, (p - 1) / factors.get(i), p) == 1){
+        for(int j = 0; j < numFactors; j++){
+            if(modExp(g, check.divide(factors.get(j)), p).equals(BigInteger.ONE)){
                 return false;
             }
         }
@@ -51,13 +61,13 @@ class PrimRootCheck {
     }
 
     //Make a fast exponentiation algorithm
-    public static long modExp(long num, long power, long mod){
-        if(power == 0) return 1;
-        if(power == 1) return num % mod;
-        if(power % 2 == 0){
-            long q = modExp(num, power/2, mod);
-            return (q * q) % mod;
+    public static BigInteger modExp(BigInteger num, BigInteger power, BigInteger mod){
+        if(power.equals(BigInteger.ZERO)) return BigInteger.ONE;
+        if(power.equals(BigInteger.ONE)) return num.mod(mod);
+        if(power.mod(BigInteger.TWO).equals(BigInteger.ZERO)){
+            BigInteger q = modExp(num, power.divide(BigInteger.TWO), mod);
+            return (q.multiply(q)).mod(mod);
         }
-        return (num * modExp(num, power - 1, mod)) % mod;
+        return (num.multiply(modExp(num, power.subtract(BigInteger.ONE), mod)).mod(mod));
     }
 }
